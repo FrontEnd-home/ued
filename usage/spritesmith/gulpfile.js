97f8453a19016d4833cc1787a3835eedfba263dd
cloudyan <1395093509@qq.com>
2015-01-06 22:03:47 +0800

    测试雪碧图

--- usage/spritesmith/gulpfile.js
+++ usage/spritesmith/gulpfile.js
@@ -7,17 +7,26 @@ var csso = require('gulp-csso');
 var imagemin = require('gulp-imagemin');
 var spritesmith = require('gulp.spritesmith');
 
+//????
+var templater = require('spritesheet-templates');
 
-var sprite_files = './test/ui'
+
+var sprite_files = './test/ui/'
     sprite_output_path = './public/';
 
 // 雪碧图任务 合并icon图标
 gulp.task('sprite', function () {
   // Generate our spritesheet
-  var spriteData = gulp.src(sprite_files+'/*.png')
+  var spriteData = gulp.src(sprite_files+'*/16.png')
       .pipe(spritesmith({
         imgName: 'icons.png',
-        cssName: '_icons.css'
+        cssName: '_icons.css',
+        //engine: phantomjssmith
+        cssSelector: function (item) { return 'selector'; },
+        //cssTemplate: 'scss.template.mustache',
+        cssVarMap: function (item) {
+          item.name = 'sprite_' + item.name;
+        }
       }));
   
   spriteData.pipe(gulp.dest(sprite_output_path+'images'));
@@ -32,6 +41,7 @@ gulp.task('sprite', function () {
   // spriteData.css
   //   .pipe(csso())
   //   .pipe(gulp.dest(sprite_output_path+'/css/'));
+
 });
 
 
